435f9e523eb9888040fdf94231454f261cc654e7
Gabbyton <gponon2000@gmail.com>
2021-04-03 19:21:18 -0400

    added functional calendar layout layout width adjusts with screen width

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
+  imgWidth: any;
+  events: any[];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.imgWidth = this.getDisplayWidth(this.getScreenInnerWidth());
+    this.events = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.imgWidth = this.getDisplayWidth(this.getScreenInnerWidth());
+    console.log(window.innerWidth);
+    console.log(this.imgWidth);
+  }
+
+  private getDisplayWidth(screenWidth: number): number {
+    console.log(this.getScreenInnerWidth());
+    let computedWidth = (0.07 * screenWidth) + 142.86;
+    console.log(computedWidth);
+    return computedWidth;
+  }
+
+  private getScreenInnerWidth(): number {
+    return window.innerWidth;
+  }
 }
