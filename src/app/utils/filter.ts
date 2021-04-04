4aae030cc8456ae89f611f7c346b15254ebcbb67
Gabbyton <gponon2000@gmail.com>
2021-04-04 08:18:40 -0400

    added icon class

---
+++ src/app/utils/filter.ts
@@ -0,0 +1,9 @@
+export class Filter {
+    name: string;
+    iconClass: string;
+
+    constructor(name: string, iconClass:string) {
+        this.name = name;
+        this.iconClass = iconClass;
+    }
+}
