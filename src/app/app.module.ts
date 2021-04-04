08e283fc3c6c7d44f87a5aeb877416f24a5ce889
Gabbyton <gponon2000@gmail.com>
2021-04-04 08:56:44 -0400

    added blank filter select component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FilterSelectComponent } from './filter-select/filter-select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarComponent
+    CalendarComponent,
+    FilterSelectComponent
   ],
   imports: [
     BrowserModule,
