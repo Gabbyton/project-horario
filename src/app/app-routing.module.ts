81e096c0401be0da66821ebf28f8a3790ef53f1c
Gabbyton <gponon2000@gmail.com>
2021-04-04 22:16:01 -0400

    added routing to calendar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddEventComponent } from './pages/add-event-view/add-event/add-event.component';
+import { CalendarComponent } from './pages/calendar-view/calendar/calendar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'calendar', component: CalendarComponent },
+  { path: 'add-event', component: AddEventComponent },
+  { path: '', redirectTo: '/calendar', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
